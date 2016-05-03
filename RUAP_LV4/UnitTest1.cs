895749501adf066e12f1e345795fa3db3cf303e0
sradonic <stjepan.radonic@gmail.com>
2016-05-03 09:38:21 +0200

    Inital commit

---
+++ RUAP_LV4/UnitTest1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class DEMOtest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://demo.opencart.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheDEMOTest()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 1000);
+
+            driver.Navigate().GoToUrl(baseURL + "/");
+            driver.FindElement(By.XPath("//div[@id='top-links']/ul/li[2]/a/span")).Click();
+            driver.FindElement(By.LinkText("Register")).Click();
+            driver.FindElement(By.Id("input-firstname")).Clear();
+            driver.FindElement(By.Id("input-firstname")).SendKeys("Ime");
+            driver.FindElement(By.Id("input-lastname")).Clear();
+            driver.FindElement(By.Id("input-lastname")).SendKeys("Prezime");
+            driver.FindElement(By.Id("input-email")).Clear();
+            driver.FindElement(By.Id("input-email")).SendKeys("moj" + randomNumber + "@email.com");
+            // ERROR: Caught exception [ERROR: Unsupported command [getEval |  | ]]
+            driver.FindElement(By.Id("input-telephone")).Clear();
+            driver.FindElement(By.Id("input-telephone")).SendKeys("93890127831");
+            driver.FindElement(By.Id("input-address-1")).Clear();
+            driver.FindElement(By.Id("input-address-1")).SendKeys("Neka adresa 1");
+            driver.FindElement(By.Id("input-city")).Clear();
+            driver.FindElement(By.Id("input-city")).SendKeys("Osijek");
+            driver.FindElement(By.Id("input-postcode")).Clear();
+            driver.FindElement(By.Id("input-postcode")).SendKeys("31000");
+            Thread.Sleep(2000);
+            new SelectElement(driver.FindElement(By.Id("input-country"))).SelectByText("Croatia");
+            Thread.Sleep(2000);
+            new SelectElement(driver.FindElement(By.Id("input-zone"))).SelectByText("Osječko-baranjska");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("input-password")).Clear();
+            driver.FindElement(By.Id("input-password")).SendKeys("Password1");
+            driver.FindElement(By.Id("input-confirm")).Clear();
+            driver.FindElement(By.Id("input-confirm")).SendKeys("Password1");
+            driver.FindElement(By.XPath("(//input[@name='newsletter'])[2]")).Click();
+            driver.FindElement(By.Name("agree")).Click();
+            driver.FindElement(By.CssSelector("input.btn.btn-primary")).Click();
+            driver.FindElement(By.LinkText("Continue")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
